612ab139fd2adf5fe01005f9fa0be0ab28a784e4
Ron DiNapoli <ron@sandcrater.com>
2020-03-09 06:59:50 -0400

    Added more functionality

--- coolScript.sh
+++ coolScript.sh
@@ -3,3 +3,12 @@
 # This is coolScript.sh, a really cool script
 #
 echo "coolScript.sh has been called with argument $1"
+
+if [[ -d $1 ]]; then
+	echo "You called coolScript.sh with a directory!"
+elif [[ -f $1 ]]; then
+	echo "You called coolScript.sh with a file!"
+else 
+	echo "You called coolScript.sh with something else!"
+fi
+	
